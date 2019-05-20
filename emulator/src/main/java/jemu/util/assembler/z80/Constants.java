e40e35eb838dcbb1736ec74d3414c660f9e3b467
Christian Wahlmann <christian@dreierschach.de>
2019-05-20 13:01:26 +0200

    emulator: fix another bug in Assembler; HalloWelt2 ist nun ein springender Ball ;-)

--- emulator/src/main/java/jemu/util/assembler/z80/Constants.java
+++ emulator/src/main/java/jemu/util/assembler/z80/Constants.java
@@ -32,7 +32,7 @@ public interface Constants {
 	}
 
 	public enum RegisterToken {
-		B("^B$", 0), C("^C$", 1), D("^D$", 2), E("^E$", 3), H("^B$", 4), IX_H("^IXh$", 4), IY_H("^IYh$", 4), L("^B$",
+		B("^B$", 0), C("^C$", 1), D("^D$", 2), E("^E$", 3), H("^H$", 4), IX_H("^IXh$", 4), IY_H("^IYh$", 4), L("^L$",
 				5), IX_L("^IXl$", 5), IY_L("^IYl$", 5), OF_HL("^(\\s*HL\\s*)$", 6), A("^A$", 7);
 
 		RegisterToken(String regex, int code) {
@@ -58,11 +58,15 @@ public interface Constants {
 
 	public enum NumToken {
 		// @formatter:off
-		hex("0x[0-9a-fA-F]{2,4}", s -> Integer.valueOf(s.substring(2), 16)), dez("(\\+|\\-){0,1}[0-9]+",
-				s -> Integer.valueOf(s, 10)), okt("0o[0-7]+", s -> Integer.valueOf(s.substring(2), 8)), bin(
-						"0b[0-1]{8,16}", s -> Integer.valueOf(s.substring(2), 2)), IXY_O("I[XY].*[+-]{1}.*[0-9]+",
-								s -> Integer.valueOf(s.substring(2))), CHR("'.'", s -> (int) s.charAt(1)), CHR2("'..'",
-										s -> (int) s.charAt(1) + ((int) s.charAt(2) << 8));
+		hex("0x[0-9a-fA-F]{2,4}", s -> Integer.valueOf(s.substring(2), 16)), 
+		dez("(\\+|\\-){0,1}[0-9]+",
+				s -> Integer.valueOf(s, 10)), 
+		okt("0o[0-7]+", s -> Integer.valueOf(s.substring(2), 8)), 
+		qua("0q[0-3]{4,8}", s -> Integer.valueOf(s.substring(2), 4)), 
+		bin("0b[0-1]{8,16}", s -> Integer.valueOf(s.substring(2), 2)), 
+		IXY_O("I[XY].*[+-]{1}.*[0-9]+", s -> Integer.valueOf(s.substring(2))), 
+		CHR("'.'", s -> (int) s.charAt(1)), 
+		CHR2("'..'", s -> (int) s.charAt(1) + ((int) s.charAt(2) << 8));
 		// @formatter:on
 		NumToken(String regex, Function<String, Integer> mapper) {
 			this.regex = regex;
