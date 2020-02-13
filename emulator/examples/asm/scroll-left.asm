50b4bf46d726c789fb6ac62cbc8eaa9812dcda87
Christian Wahlmann <christian@dreierschach.de>
2020-02-13 22:05:27 +0100

    Doku

--- emulator/examples/asm/scroll-left.asm
+++ emulator/examples/asm/scroll-left.asm
@@ -1,35 +1,35 @@
-.org 0x7200
-.run 0x7200
+.org 0x7200         // Programm in Grafikspeicher laden
+.run 0x7200         // und dort auch starten
 
-.def screen: 0x7000
+.def screen: 0x7000 // Bildschirmspeicheradresse
 
-LD HL, screen:
-LD DE, screen:
+LD HL, screen:      // HL = Bildschirm Startadresse
+LD DE, screen:      // DE = Bildschirm Startadresse + 1
+INC DE      
+LD BC, 0x01ff       // BC = Zähler
+
+loop:               // Schleife 1
+
+LD A, (DE)          // Zeichen aus DE laden
+LD (HL), A          // und nach HL schreiben
+INC HL              // Adresspointer erhöhen
 INC DE
-LD BC, 0x01ff
-
-loop:
-
-LD A, (DE)
-LD (HL), A
-INC HL
-INC DE
-DEC BC
-LD A, B
+DEC BC              // Zähler - 1
+LD A, B             // Zähler = 0 ?
 OR C
-JR NZ, loop:
+JR NZ, loop:        // nein, weiter
 
-LD HL, screen:
-LD DE, 0x0020
-ADD HL, DE
-DEC HL
-LD B, 0x10
-LD A, 0x20
+LD HL, screen:      // HL = Bildschirm Startadresse
+LD DE, 0x0020       // DE = Zeilenlänge
+ADD HL, DE          // HL + Zeilenlänge - 1 = Ende der 1. Zeile 
+DEC HL             
+LD B, 0x10          // B = Zähler für 16 Zeilen
+LD A, 0x20          // A = Leerzeichen
 
-loop2:
-
-LD (HL), A
-ADD HL, DE
-DEC B
-JR NZ, loop2:
-RET
+loop2:              // Schleife
+   
+LD (HL), A          // Leerzeichen schreiben
+ADD HL, DE          // nächste Zeile
+DEC B               // Zähler - 1
+JR NZ, loop2:       // weiter, solange Zähler != 0
+RET                 // Ende
